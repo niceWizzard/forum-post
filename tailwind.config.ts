1cfe27e99ebfd7a944227b6203840094d12a10c6
Richard <richard232manansala@gmail.com>
2024-06-28 20:12:34 +0800

    Added some colors theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,56 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          "50": "#eefbfd",
+          "100": "#d3f4fa",
+          "200": "#ade8f4",
+          "300": "#75d7eb",
+          "400": "#35bcdb",
+          "500": "#1a9fc0",
+          "600": "#1880a2",
+          "700": "#1c6e8c",
+          "800": "#1e556c",
+          "900": "#1d485c",
+          "950": "#0e2e3e",
+          DEFAULT: "#1c6e8c",
+        },
+        surface: {
+          "50": "#f8f7f5",
+          "100": "#e7e6e0",
+          "200": "#cfccc0",
+          "300": "#b0ad98",
+          "400": "#8f8c72",
+          "500": "#747258",
+          "600": "#5c5b45",
+          "700": "#4b4a3a",
+          "800": "#3e3d31",
+          "900": "#35352c",
+          "950": "#040403",
+          DEFAULT: "#040403",
+        },
+
+        danger: {
+          "50": "#fff1f2",
+          "100": "#ffe3e6",
+          "200": "#ffccd3",
+          "300": "#ffa2b1",
+          "400": "#fe6781",
+          "500": "#f83b60",
+          "600": "#e5194c",
+          "700": "#c20e3f",
+          "800": "#a20f3c",
+          "900": "#8b1039",
+          "950": "#4e031a",
+          DEFAULT: "#e5194c",
+        },
+
+        on: {
+          primary: "#FDF7FA",
+          surface: "#FDF7FA",
+          danger: "#FDF7FA",
+        },
       },
     },
   },
