c729ed57c5304501c30988cd687ac8aa7232d13f
Richard <richard232manansala@gmail.com>
2024-06-29 09:14:49 +0800

    Designed the barebones footer

--- src/app/(root)/_components/footer.tsx
+++ src/app/(root)/_components/footer.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-card shadow-md">
+      <div className="container px-4 py-4">
+        <h4 className="text-lg font-semibold mb-4"> Forum Post</h4>
+        <span>All rights unreserved.</span>
+      </div>
+    </footer>
+  );
 }
