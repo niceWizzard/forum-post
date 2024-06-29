b236f3fd6ad3735dc0e1d09eece4460ad023617c
Richard <richard232manansala@gmail.com>
2024-06-29 08:35:47 +0800

    Changed the theming stucture again

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import React from "react";
 
 export default function LoginPage() {
   return (
     <section className="w-full h-full flex justify-center px-4 py-2 flex-col items-center gap-4">
-      <div className=" w-96 h-96 border border-gray-400 text-center gap-3 px-2 py-4 rounded-md flex flex-col">
+      <div className="bg-surface-accent w-96 h-96  text-center gap-3 px-2 py-4 rounded-md flex flex-col">
         <h3 className="font-semibold text-xl border-b pb-4 border-gray-400">
           Log in or Create your account
         </h3>
@@ -13,14 +12,14 @@ export default function LoginPage() {
           providers
         </p>
         <div className="flex flex-col items-stretch justify-center gap-4">
-          <button className="bg-primary-800 px-4 py-2 rounded-md ">
+          <button className="bg-primary px-4 py-2 rounded-md ">
             Continue with Google
           </button>
-          <button className="bg-primary-800 px-4 py-2 rounded-md ">
+          <button className="bg-primary px-4 py-2 rounded-md ">
             Continue with Github
           </button>
         </div>
-        <p className="text-xs font-light text-gray-100 flex-grow content-end">
+        <p className="text-xs font-light text-gray-300 flex-grow content-end">
           Tip: To create an account, you need to continue with one of the social
           media providers
         </p>
