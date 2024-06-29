ac196ceec0a1f407a0d6e969b713d053777c9d4b
Richard <richard232manansala@gmail.com>
2024-06-29 21:05:50 +0800

    Changed the theme colors again

--- src/app/(root)/_components/header.tsx
+++ src/app/(root)/_components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="border-b-2 border-accent ">
+    <header className="border-b-2  ">
       <div className="w-full mx-auto container flex justify-between px-2 py-4 items-center">
         <h1 className="text-xl font-semibold">Forum Poster</h1>
         <nav className="flex gap-3 items-center">
