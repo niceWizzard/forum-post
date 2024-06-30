15cec590429a9105ed571efe3bff16ea82258622
Richard <richard232manansala@gmail.com>
2024-06-30 23:26:24 +0800

    Removed github env

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -2,12 +2,6 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    GITHUB_CLIENT_ID: z.string().min(1, "Github Client ID is required."),
-    GITHUB_SECRET: z.string().min(1, "Github Secret is required."),
-  },
-  runtimeEnvStrict: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-  },
+  server: {},
+  runtimeEnvStrict: {},
 });
