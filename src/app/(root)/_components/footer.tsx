f367711757ec6c9effbc15e4a70c65f6e6faf096
Richard <richard232manansala@gmail.com>
2024-06-30 20:11:06 +0800

    Finished footer

--- src/app/(root)/_components/footer.tsx
+++ src/app/(root)/_components/footer.tsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-card shadow-md">
-      <div className="container px-4 py-4">
+    <footer className="shadow-md border-t">
+      <div className="container px-4 py-4 flex flex-col gap-4">
         <h4 className="text-lg font-semibold mb-4"> Forum Post</h4>
-        <span>All rights unreserved.</span>
+        <div className="flex gap-2 ">
+          <ul className="flex flex-col">
+            <Link href="/about">About Us</Link>
+            <Link href="/contact">Contact Us</Link>
+            <Link href="#">Sitemap</Link>
+            <Link href="#">Terms of Service</Link>
+          </ul>
+          <ul className="flex flex-col ">
+            <Link href="#">Facebook</Link>
+            <Link href="#">Twitter</Link>
+            <Link href="#">Instagram</Link>
+            <Link href="#">Reddit</Link>
+          </ul>
+        </div>
+        <p className="text-center font-light">
+          All rights unreserved. Designed & Developed by Code Rizzard @ 2024
+        </p>
       </div>
     </footer>
   );
