091b83ca02d2e598f2a1d41b9ce3d6e4bb6a3262
Richard <richard232manansala@gmail.com>
2024-06-30 22:41:37 +0800

    Removed NEXT_AUTH_SECRET

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -5,11 +5,9 @@ export const env = createEnv({
   server: {
     GITHUB_CLIENT_ID: z.string().min(1, "Github Client ID is required."),
     GITHUB_SECRET: z.string().min(1, "Github Secret is required."),
-    NEXT_AUTH_SECRET: z.string().min(1),
   },
   runtimeEnvStrict: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
-    NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
   },
 });
