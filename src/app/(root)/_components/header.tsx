a4b75302e7fa628cf1c66e0dd8911890c65ce419
Richard <richard232manansala@gmail.com>
2024-06-30 23:28:07 +0800

    Added signout button

--- src/app/(root)/_components/header.tsx
+++ src/app/(root)/_components/header.tsx
@@ -1,15 +1,28 @@
+import { auth, signOut } from "@/auth";
 import Link from "next/link";
 import React from "react";
 
-const Header = () => {
+const Header = async () => {
+  const user = await auth();
   return (
     <header className="border-b-2  ">
       <div className="w-full mx-auto container flex justify-between px-2 py-4 items-center">
         <h1 className="text-xl font-semibold">Forum Poster</h1>
         <nav className="flex gap-3 items-center">
-          <Link href="/login" className="underlined">
-            Login
-          </Link>
+          {user ? (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button>Logout</button>
+            </form>
+          ) : (
+            <Link href="/login" className="underlined">
+              Login
+            </Link>
+          )}
         </nav>
       </div>
     </header>
