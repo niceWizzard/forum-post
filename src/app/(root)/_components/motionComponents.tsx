c949611ed602286385c5396fc5669a569b392f3c
Richard <richard232manansala@gmail.com>
2024-06-30 12:21:29 +0800

    Added motion p & h2 components

--- src/app/(root)/_components/motionComponents.tsx
+++ src/app/(root)/_components/motionComponents.tsx
@@ -11,3 +11,10 @@ type MotionElementProps<T extends keyof ReactHTML> = HTMLMotionProps<T> & Props;
 export function MotionDiv({ children, ...rest }: MotionElementProps<"div">) {
   return <motion.div {...rest}>{children}</motion.div>;
 }
+
+export function MotionH2({ children, ...rest }: MotionElementProps<"h2">) {
+  return <motion.h2 {...rest}>{children}</motion.h2>;
+}
+export function MotionP({ children, ...rest }: MotionElementProps<"p">) {
+  return <motion.p {...rest}>{children}</motion.p>;
+}
