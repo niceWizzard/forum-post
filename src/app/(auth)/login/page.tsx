40b44c44ffc268bcd751bf98d5a5c73a789a6573
Richard <richard232manansala@gmail.com>
2024-06-30 23:26:41 +0800

    Configured the next-auth beta

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,8 @@
+import { auth, signIn } from "@/auth";
 import Link from "next/link";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await auth();
   return (
     <section className="w-full h-full flex justify-center px-4 py-12 flex-col items-center gap-4">
       <div className="bg-card text-card-foreground w-96 text-justify h-full   gap-3 px-6 py-4 rounded-md flex flex-col">
@@ -11,17 +13,26 @@ export default function LoginPage() {
           To continue with services offered in our website, you need to login
           with your account first.
         </p>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+
         <p className="mt-4 font-light">
           You can choose to login with the following:
         </p>
-        <form className="flex flex-col gap-4 flex-grow justify-center">
+        <div className="flex flex-col gap-4 flex-grow justify-center">
           <button className="px-3 py-3 text-lg font-medium rounded-md w-full bg-gray-300 text-gray-900">
             Login with Google
           </button>
-          <button className="px-3 py-3 text-lg font-medium border rounded-md w-full">
-            Login with Github
-          </button>
-        </form>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            <button className="px-3 py-3 text-lg font-medium border rounded-md w-full">
+              Login with Github
+            </button>
+          </form>
+        </div>
       </div>
       <div className="flex">
         <Link className="underlined" href="/">
