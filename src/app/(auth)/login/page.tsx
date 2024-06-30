09dce5f01268bf87d46822cf0ade9c51d3f87b3f
Richard <richard232manansala@gmail.com>
2024-06-30 20:19:46 +0800

    Improved the login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,26 +3,25 @@ import Link from "next/link";
 export default function LoginPage() {
   return (
     <section className="w-full h-full flex justify-center px-4 py-12 flex-col items-center gap-4">
-      <div className="bg-card text-card-foreground w-128 h-full  text-center gap-3 px-6 py-4 rounded-md flex flex-col">
-        <h3 className="font-semibold text-xl border-b pb-4 border-gray-400">
-          Log in or Create your account
-        </h3>
-        <p className="flex-grow">
-          You can continue with your account with the following social media
-          providers
+      <div className="bg-card text-card-foreground w-96 text-justify h-full   gap-3 px-6 py-4 rounded-md flex flex-col">
+        <h2 className="text-xl font-semibold pb-4 border-b text-center">
+          Login to your account
+        </h2>
+        <p className="text-sm font-light">
+          To continue with services offered in our website, you need to login
+          with your account first.
         </p>
-        <div className="flex flex-col items-stretch justify-start gap-4 flex-grow ">
-          <button className="bg-primary px-4 py-2 rounded-md h-16 text-lg text-start">
-            Continue with Google
-          </button>
-          <button className="bg-primary px-4 py-2 rounded-md h-16 text-lg text-start">
-            Continue with Github
-          </button>
-        </div>
-        <p className="text-xs font-light text-gray-300 flex-grow content-end">
-          Tip: To create an account, you need to continue with one of the social
-          media providers
+        <p className="mt-4 font-light">
+          You can choose to login with the following:
         </p>
+        <form className="flex flex-col gap-4 flex-grow justify-center">
+          <button className="px-3 py-3 text-lg font-medium rounded-md w-full bg-gray-300 text-gray-900">
+            Login with Google
+          </button>
+          <button className="px-3 py-3 text-lg font-medium border rounded-md w-full">
+            Login with Github
+          </button>
+        </form>
       </div>
       <div className="flex">
         <Link className="underlined" href="/">
