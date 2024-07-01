cb045fc542a1ecdf214727ca46f2ef10b21614a6
Richard <richard232manansala@gmail.com>
2024-07-01 12:02:26 +0800

    Initialize drizzle db

---
+++ src/server/db/index.ts
@@ -0,0 +1,7 @@
+import { env } from "@/env/server.mjs";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const client = postgres(env.NEON_DATABASE_URL);
+
+export const db = drizzle(client);
