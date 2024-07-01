ecc0b823bb2510903b4eafce3807461464d4bdc4
Richard <richard232manansala@gmail.com>
2024-07-01 11:53:38 +0800

    Added GITHUB variables

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -4,8 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NEON_DATABASE_URL: z.string().url("DATABASE URL IS REQUIRED."),
+    GITHUB_CLIENT_ID: z.string().min(1, "GITHUB CLIENT ID IS REQUIRED."),
+    GITHUB_CLIENT_SECRET: z
+      .string()
+      .min(1, "GITHUB CLIENT SECRET IS REQUIRED."),
   },
   runtimeEnvStrict: {
     NEON_DATABASE_URL: process.env.NEON_DATABASE_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
 });
