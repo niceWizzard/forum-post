ca6fb470cd7019265e7eebbfdacd5fbf3c663747
Richard <richard232manansala@gmail.com>
2024-07-01 10:46:13 +0800

    Added NEON_DATABASE_URL

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -2,6 +2,10 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {},
-  runtimeEnvStrict: {},
+  server: {
+    NEON_DATABASE_URL: z.string().url("DATABASE URL IS REQUIRED."),
+  },
+  runtimeEnvStrict: {
+    NEON_DATABASE_URL: process.env.NEON_DATABASE_URL,
+  },
 });
