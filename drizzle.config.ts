51e7ec27ef1278f1225482ded42ba17b1031d71c
Richard <richard232manansala@gmail.com>
2024-07-01 11:54:07 +0800

    Created drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env/server.mjs";
+
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/server/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.NEON_DATABASE_URL,
+  },
+});
