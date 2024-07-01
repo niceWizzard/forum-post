008e01b8d75dfe0b6aa61559d381111cd7e4cb49
Richard <richard232manansala@gmail.com>
2024-07-01 15:33:21 +0800

    Add route protection for already onboarded users

--- src/app/(root)/onboarding/page.tsx
+++ src/app/(root)/onboarding/page.tsx
@@ -1,9 +1,13 @@
 import { authenticatedOnly } from "@/server/auth/validate";
 import React from "react";
 import OnBoardingForm from "./_components/onboardingForm";
+import { redirect } from "next/navigation";
 
 export default async function OnboardingPage() {
   const { user } = await authenticatedOnly({ onboardCheck: false });
+  if (user.name != null || user.username != null) {
+    redirect("/feed");
+  }
 
   return (
     <section className="py-4 h-screen">
