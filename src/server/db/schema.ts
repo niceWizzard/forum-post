8b11fa20ef5d43cfb6b91a570f95c5f6e39343ea
Richard <richard232manansala@gmail.com>
2024-07-01 13:17:57 +0800

    Made the sessiontable userId be type uuid

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,13 +2,13 @@ import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
   id: uuid("id").primaryKey().defaultRandom(),
-  username: text("username").notNull().unique(),
+  username: text("username").unique(),
   github_id: text("github_id").unique(),
 });
 
 export const sessionTable = pgTable("session", {
   id: text("id").notNull().primaryKey(),
-  userId: text("user_id")
+  userId: uuid("user_id")
     .notNull()
     .references(() => userTable.id),
   expiresAt: timestamp("expires_at").notNull(),
