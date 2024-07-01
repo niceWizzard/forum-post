86351614a1ea3e0ba42a48a30a8178e003264029
Richard <richard232manansala@gmail.com>
2024-07-01 18:26:58 +0800

    Showed created forums of the user

--- src/app/(root)/feed/page.tsx
+++ src/app/(root)/feed/page.tsx
@@ -1,14 +1,17 @@
 import { validateRequest } from "@/server/auth/action";
 import { authenticatedOnly } from "@/server/auth/validate";
+import { getCreatedForums } from "@/server/db/actions/forum";
 import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function FeedPage() {
   const { user } = await authenticatedOnly();
 
+  const createdForums = await getCreatedForums(user.id);
+
   return (
     <div>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>{JSON.stringify(createdForums, null, 2)}</pre>
     </div>
   );
 }
