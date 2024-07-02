6eeda292ed65a9b6661519aca1c3eaa019191480
Richard <richard232manansala@gmail.com>
2024-07-02 22:06:12 +0800

    Added clearUser in store

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -6,9 +6,11 @@ import { create } from "zustand";
 interface UserStore {
   user: User | null;
   setUser: (v: User | null) => void;
+  clearUser: () => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
   setUser: (v) => set({ user: v }),
+  clearUser: () => set({ user: null }),
 }));
