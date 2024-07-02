7a75ef6a30dddf79da89f8fe8a953d7915601f41
Richard <richard232manansala@gmail.com>
2024-07-02 21:56:17 +0800

    Made the title page in header a link to home

--- src/app/(root)/_components/header.tsx
+++ src/app/(root)/_components/header.tsx
@@ -1,12 +1,15 @@
 import { validateRequest } from "@/server/auth/action";
 import HeaderNav from "./headerNav";
+import Link from "next/link";
 
 const Header = async () => {
   const { user } = await validateRequest();
   return (
     <header className="border-b-2  ">
       <div className="w-full mx-auto container flex justify-between px-2 py-4 items-center">
-        <h1 className="text-xl font-semibold">Forum Poster</h1>
+        <Link href="/">
+          <h1 className="text-xl font-semibold">Forum Poster</h1>
+        </Link>
         <HeaderNav user={user} />
       </div>
     </header>
