8c223f5b6debcfad67afa2cb5ff5b2402ab7c9d0
Richard <richard232manansala@gmail.com>
2024-07-04 16:44:25 +0800

    Added header for the forum page

--- src/app/(root)/forum/[id]/page.tsx
+++ src/app/(root)/forum/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { getForumById } from "@/server/db/queries/forum";
+import { forumTable } from "@/server/db/schema";
+import * as schema from "@/server/db/schema";
+import { InferSelectModel } from "drizzle-orm";
 
 interface Props {
   params: { id: string };
@@ -9,12 +12,32 @@ const ForumWithIdPage = async ({ params: { id } }: Props) => {
   if (!forum) {
     return "Invalid forum";
   }
+
   return (
-    <div>
-      <h3>{forum.name}</h3>
-      <span>Created at {new Date(forum.createdAt).toLocaleString()}</span>
-    </div>
+    <section>
+      <ForumHeader forum={forum} />
+    </section>
   );
 };
 
 export default ForumWithIdPage;
+function ForumHeader({
+  forum,
+}: {
+  forum: InferSelectModel<typeof forumTable>;
+}) {
+  return (
+    <div className="bg-card pt-12 pb-6 px-4 overflow-hidden break-words whitespace-normal">
+      <div className="container gap-6 flex flex-col ">
+        <div className="row-span-2 flex gap-4 items-center">
+          <h3 className="text-xl font-semibold flex-grow">{forum.name}</h3>
+          <span className="">500k members</span>
+          <button className="row-span-1">Join now</button>
+        </div>
+        <p className="text-sm font-light text-gray-300 row-span-1 col-span-1 text-ellipsis ">
+          {forum.description || "nothing to see here... :)"}
+        </p>
+      </div>
+    </div>
+  );
+}
