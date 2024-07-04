8e6be28e5de5413e73bd6e3315c74cf7ac491e16
Richard <richard232manansala@gmail.com>
2024-07-04 17:33:51 +0800

    Created a loadingButton

---
+++ src/components/ui/loadingButton.tsx
@@ -0,0 +1,30 @@
+import { Loader2 } from "lucide-react";
+import { Button, ButtonProps } from "./button";
+
+interface LoadingButtonProps extends ButtonProps {
+  isLoading?: boolean;
+  loadingText?: string;
+  disabledOnLoading?: boolean;
+  loadingIcon?: React.ReactNode;
+}
+
+export const LoadingButton = ({
+  isLoading,
+  loadingText,
+  disabledOnLoading,
+  loadingIcon,
+  disabled,
+  ...props
+}: LoadingButtonProps) => {
+  const loadingComp = (
+    <span className="flex items-center justify-center">
+      {loadingIcon ?? <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+      {loadingText ?? "Loading..."}
+    </span>
+  );
+  return (
+    <Button disabled={disabledOnLoading ? isLoading : disabled} {...props}>
+      {isLoading ? loadingComp : props.children}
+    </Button>
+  );
+};
