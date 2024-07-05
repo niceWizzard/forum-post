b75521e3b440a064b1e5022da1e570e8078b30e1
Richard <richard232manansala@gmail.com>
2024-07-05 11:26:45 +0800

    Added PrivateUser type and a utility to remove sensitive info to public User type

--- src/server/db/schema/types.ts
+++ src/server/db/schema/types.ts
@@ -1,6 +1,16 @@
 import { InferSelectModel } from "drizzle-orm";
 import { forumTable } from "./forum";
 import { postTable } from "./post";
+import { userTable } from ".";
+
+type StrictOmit<T, K extends keyof T> = Omit<T, K>;
 
 export type Forum = InferSelectModel<typeof forumTable>;
 export type Post = InferSelectModel<typeof postTable>;
+export type PrivateUser = InferSelectModel<typeof userTable>;
+export type User = StrictOmit<PrivateUser, "github_id">;
+
+export function exposeUserType(user: PrivateUser): User {
+  const { github_id, ...output } = user;
+  return output;
+}
