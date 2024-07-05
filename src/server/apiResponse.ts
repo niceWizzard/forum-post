ad5a091405523a962d513636d7448e7151e5bfec
Richard <richard232manansala@gmail.com>
2024-07-05 14:24:26 +0800

    Made helper util class for server responses

--- src/server/apiResponse.ts
+++ src/server/apiResponse.ts
@@ -1,11 +1,29 @@
 export type ApiResponse<T = any> =
   | {
       error: false;
-      message: string;
-      data?: T;
+      message?: string;
+      data: T;
     }
   | {
       error: true;
       message: string;
       code: number;
     };
+
+export abstract class ApiRes {
+  private constructor() {}
+
+  static success<T>(params: { data: T; message?: string }): ApiResponse<T> {
+    return {
+      error: false,
+      ...params,
+    };
+  }
+
+  static error(params: { message: string; code: number }): ApiResponse<any> {
+    return {
+      error: true,
+      ...params,
+    };
+  }
+}
