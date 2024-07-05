7a144a5d7d13d707b8e9d89d70ff0a19f0df60be
Richard <richard232manansala@gmail.com>
2024-07-05 13:26:20 +0800

    Added some placeholder ui for the post:id page

--- src/app/(root)/post/[id]/page.tsx
+++ src/app/(root)/post/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getPostById } from "@/server/db/queries/post";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -11,9 +12,32 @@ export default async function PostPage({ params: { id } }: Props) {
   }
 
   return (
-    <section>
-      <div className="container">
-        <pre>{JSON.stringify(res, null, 4)}</pre>
+    <section className="py-6">
+      <div className="container flex flex-col">
+        <div className="flex-flex-col bg-card px-4 py-2">
+          <h3>{res.title}</h3>
+          <div className="flex gap-3 text-foreground-lighter font-light text-sm items-center mt-2">
+            <Link href={`/forum/${res.forumId}`} className="underline">
+              f/{res.forum.name}
+            </Link>
+            &middot;
+            <Link href={`/profile/${res.posterId}`} className="underline">
+              {" "}
+              @{res.poster?.username ?? "deleted"}
+            </Link>
+            <span className="text-xs">
+              {new Date(res.createdAt).toLocaleString()}
+            </span>
+          </div>
+        </div>
+        <div className="py-2 flex flex-col gap-4">
+          <p className="text-md px-4 ">{res.body}</p>
+          <div className="flex gap-3 border-t border-t-foreground-light py-4">
+            <button>Like</button>
+            <button>Comment</button>
+            <button>Share</button>
+          </div>
+        </div>
       </div>
     </section>
   );
