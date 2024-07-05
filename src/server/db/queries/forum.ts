71c503d44cf355c284020c10348ef4c6b584eeed
Richard <richard232manansala@gmail.com>
2024-07-05 11:30:28 +0800

    Put the queries in cache

--- src/server/db/queries/forum.ts
+++ src/server/db/queries/forum.ts
@@ -3,34 +3,35 @@ import { db } from "..";
 import { eq } from "drizzle-orm";
 import { forumMemberTable, forumTable } from "../schema/forum";
 import { postTable } from "../schema/post";
+import { cache } from "react";
 
-export async function getJoinedForums(userId: string) {
+export const getJoinedForums = cache(async (userId: string) => {
   const joinedForums = await db.query.forumMemberTable.findMany({
     where: eq(forumMemberTable.userId, userId),
   });
   return joinedForums;
-}
+});
 
-export async function getCreatedForums(userId: string) {
+export const getCreatedForums = cache(async (userId: string) => {
   const createdForums = await db.query.forumTable.findMany({
     where: eq(forumTable.ownerId, userId),
   });
 
   return createdForums;
-}
+});
 
-export async function getForumById(forumId: string) {
+export const getForumById = cache(async (forumId: string) => {
   const forum = await db.query.forumTable.findFirst({
     where: eq(forumTable.id, forumId),
   });
 
   return forum;
-}
+});
 
-export async function getForumPosts(forumId: string) {
+export const getForumPosts = cache(async (forumId: string) => {
   const posts = await db.query.postTable.findMany({
     where: eq(postTable.forumId, forumId),
   });
 
   return posts;
-}
+});
--- src/server/db/queries/post.ts
+++ src/server/db/queries/post.ts
@@ -4,33 +4,37 @@ import { postTable } from "../schema/post";
 import { eq } from "drizzle-orm";
 import { userTable } from "../schema";
 import { exposeUserType, Post, PrivateUser, User } from "../schema/types";
+import { cache } from "react";
 
 interface PostWithUser extends Post {
   poster: User | null;
 }
-export const getPostById = async <T extends boolean>(
-  id: string,
-  withOwner: T = false as T
-): Promise<T extends true ? PostWithUser | null : Post | null> => {
-  if (withOwner) {
-    const res = await db
-      .select()
-      .from(postTable)
-      .where(eq(postTable.id, id))
-      .leftJoin(userTable, eq(userTable.id, postTable.posterId));
+export const getPostById = cache(
+  async <T extends boolean>(
+    id: string,
+    withOwner: T = false as T
+  ): Promise<T extends true ? PostWithUser | null : Post | null> => {
+    console.log("GETTING POST: ", id);
+    if (withOwner) {
+      const res = await db
+        .select()
+        .from(postTable)
+        .where(eq(postTable.id, id))
+        .leftJoin(userTable, eq(userTable.id, postTable.posterId));
+      if (res.length == 0) return null;
+
+      const data = res[0];
+      const poster = data.user ? exposeUserType(data.user) : null;
+
+      return {
+        poster,
+        ...data.post,
+      } as T extends true ? PostWithUser : Post;
+    }
+
+    const res = await db.select().from(postTable).where(eq(postTable.id, id));
     if (res.length == 0) return null;
 
-    const data = res[0];
-    const poster = data.user ? exposeUserType(data.user) : null;
-
-    return {
-      poster,
-      ...data.post,
-    } as T extends true ? PostWithUser : Post;
+    return res[0] as T extends true ? PostWithUser : Post;
   }
-
-  const res = await db.select().from(postTable).where(eq(postTable.id, id));
-  if (res.length == 0) return null;
-
-  return res[0] as T extends true ? PostWithUser : Post;
-};
+);
