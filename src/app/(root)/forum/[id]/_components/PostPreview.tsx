bc763cb07b77b7e71e3398ff42e4aa9bf093de9b
Richard <richard232manansala@gmail.com>
2024-07-05 13:55:08 +0800

    Made the username & forum name linkable to their own links

--- src/app/(root)/forum/[id]/_components/PostPreview.tsx
+++ src/app/(root)/forum/[id]/_components/PostPreview.tsx
@@ -1,24 +1,50 @@
+"use client";
 import { type Post } from "@/server/db/schema/types";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function PostPreview({ post }: { post: Post }) {
+  const router = useRouter();
+  function onInnerClick(
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+    type: "forum" | "username"
+  ) {
+    e.preventDefault();
+    switch (type) {
+      case "forum":
+        router.push(`/forum/${post.forumId}`);
+        break;
+      case "username":
+        router.push(`/profile/${post.posterId}`);
+        break;
+    }
+  }
+
   return (
     <div className="py-4 overflow-hidden  px-2 ">
-      <div className="hover:bg-card px-4 py-2 transition-all duration-200">
-        <Link href={`/post/${post.id}`} key={post.id}>
-          <div className="flex gap-1 items-center text-xs text-foreground-light">
-            <span>f/{post.forum.name}</span>·
-            <span>
+      <Link href={`/post/${post.id}`}>
+        <div className="hover:bg-card px-4 py-2 transition-all duration-200">
+          <h3 className="font-semibold text-md ">{post.title}</h3>
+          <div className="flex gap-1 items-center text-xs text-foreground-lighter cursor-pointer">
+            <span
+              className="hover:underline"
+              onClick={(e) => onInnerClick(e, "forum")}
+            >
+              f/{post.forum.name}
+            </span>
+            ·
+            <span
+              className="hover:underline"
+              onClick={(e) => onInnerClick(e, "username")}
+            >
               @{post.poster ? post.poster.username ?? "no username" : "deleted"}
             </span>
           </div>
-          <h3 className="font-semibold text-md mb-4">{post.title}</h3>
-
-          <p className="text-ellipsis text-foreground-light font-light text-sm">
+          <p className="text-ellipsis text-foreground-light font-light text-sm mt-4">
             {post.body}
           </p>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
