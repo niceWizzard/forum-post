ab728460fa14594912bab35bf6df087a46ebc1e5
Richard <richard232manansala@gmail.com>
2024-07-05 12:13:59 +0800

    Made PostComplete type which have both user and forum (limited)

--- src/server/db/schema/types.ts
+++ src/server/db/schema/types.ts
@@ -8,6 +8,9 @@ type StrictOmit<T, K extends keyof T> = Omit<T, K>;
 export type Forum = InferSelectModel<typeof forumTable>;
 export type Post = InferSelectModel<typeof postTable>;
 export type PostWithPoster = Post & { poster: User | null };
+export type PostComplete = PostWithPoster & {
+  forum: Pick<Forum, "id" | "name">;
+};
 export type PrivateUser = InferSelectModel<typeof userTable>;
 export type User = StrictOmit<PrivateUser, "github_id">;
 
