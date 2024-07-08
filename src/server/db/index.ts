e273edf7aa9257796e9c041d5e416bba0e7b6024
Richard <richard232manansala@gmail.com>
2024-07-08 17:29:04 +0800

    Changed the db source to neon client

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,12 @@
 import { env } from "@/env/server.mjs";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as userSchemas from "@/server/db/schema";
 import * as commentSchemas from "@/server/db/schema/comment";
 import * as forumSchemas from "@/server/db/schema/forum";
 import * as postSchemas from "@/server/db/schema/post";
+import { neon } from "@neondatabase/serverless";
 
-const client = postgres(env.NEON_DATABASE_URL);
+const client = neon(env.NEON_DATABASE_URL);
 
 export const db = drizzle(client, {
   schema: {
