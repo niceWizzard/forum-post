3351582da4de12c99fe0c67b673239b624af9309
Richard <richard232manansala@gmail.com>
2024-07-09 18:22:47 +0800

    Created api/auth/google route

---
+++ src/app/api/auth/google/route.ts
@@ -0,0 +1,43 @@
+import { AuthFlowType, isAuthType } from "@/lib/utils.server";
+import { ApiError } from "@/server/apiErrors";
+import { ApiRes } from "@/server/apiResponse";
+import { google } from "@/server/auth/providers";
+import { CookieName } from "@/server/cookieName";
+import { generateCodeVerifier, generateState } from "arctic";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest): Promise<Response> {
+  const reqUrl = new URL(request.url);
+  const type: AuthFlowType | string | null = reqUrl.searchParams.get("type");
+
+  if (!isAuthType(type)) {
+    return NextResponse.json(
+      ApiRes.error({
+        message: "Missing / invalid parameter 'type'",
+        code: ApiError.InvalidParameter,
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const state = generateState();
+  const verifier = generateCodeVerifier();
+  const url = await google.createAuthorizationURL(state, verifier);
+
+  const options = {
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: "lax",
+  } as const;
+  cookies().set(CookieName.GOOGLE_OAUTH_STATE, state, options);
+  cookies().set(CookieName.GOOGLE_CODE_VERIFIER, state, options);
+
+  cookies().set(CookieName.OAUTH_FLOW_TYPE, type, options);
+
+  return Response.redirect(url);
+}
