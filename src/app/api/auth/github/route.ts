fdd8850226a2abcf0ac1d8b0548cb256324f612e
Richard <richard232manansala@gmail.com>
2024-07-09 16:37:01 +0800

    Used oauth flow type in github first route

--- src/app/api/auth/github/route.ts
+++ src/app/api/auth/github/route.ts
@@ -1,12 +1,33 @@
+import { AuthFlowType, isAuthType } from "@/lib/utils.server";
+import { ApiError } from "@/server/apiErrors";
+import { ApiRes } from "@/server/apiResponse";
 import { github } from "@/server/auth/providers";
 import { generateState } from "arctic";
 import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(): Promise<Response> {
+export async function GET(request: NextRequest): Promise<Response> {
+  const reqUrl = new URL(request.url);
+  const type: AuthFlowType | string | null = reqUrl.searchParams.get("type");
+
+  if (!isAuthType(type)) {
+    return NextResponse.json(
+      ApiRes.error({
+        message: "Missing / invalid parameter 'type'",
+        code: ApiError.InvalidParameter,
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
   const state = generateState();
   const url = await github.createAuthorizationURL(state, {
     scopes: ["user:email"],
   });
+
+  url.searchParams.append("type", type);
 
   cookies().set("github_oauth_state", state, {
     path: "/",
