bfd9b982cad2d98d5f09fa96dcb907ee5c54f6c8
Richard <richard232manansala@gmail.com>
2024-07-09 17:12:14 +0800

    Manually added the values of the apiErrors so they won't change later

--- src/server/apiErrors.ts
+++ src/server/apiErrors.ts
@@ -1,24 +1,24 @@
 export enum ApiError {
-  AuthRequired,
-  Unathorized,
-  CreateError,
-  PostError,
-  UpdateError,
-  DeleteError,
+  AuthRequired = 1,
+  Unathorized = 2,
+  CreateError = 3,
+  PostError = 4,
+  UpdateError = 5,
+  DeleteError = 6,
 
-  ForumNotFound,
-  PostNotFound,
-  UserNotFound,
+  ForumNotFound = 7,
+  PostNotFound = 8,
+  UserNotFound = 9,
 
-  UnknownError,
-  UsernameAlreadyExists,
-  ForumNameAlreadyExists,
+  UnknownError = 10,
+  UsernameAlreadyExists = 11,
+  ForumNameAlreadyExists = 12,
 
-  MissingParameter,
-  InvalidParameter,
+  MissingParameter = 13,
+  InvalidParameter = 14,
 
-  InvalidToken,
+  InvalidToken = 15,
 
-  UserAlreadyExists,
-  UserDoesNotExist,
+  UserAlreadyExists = 16,
+  UserDoesNotExist = 17,
 }
