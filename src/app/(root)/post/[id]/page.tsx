2017a63b4105af5fc9dc793849fcd07a56c055f3
Richard <richard232manansala@gmail.com>
2024-07-09 12:24:34 +0800

    Wrapped the response of getPostById in ApiResponse type

--- src/app/(root)/post/[id]/page.tsx
+++ src/app/(root)/post/[id]/page.tsx
@@ -8,32 +8,34 @@ interface Props {
 
 export default async function PostPage({ params: { id } }: Props) {
   const res = await getPostById(id);
-  if (!res) {
+  if (res.error) {
     return "Invalid post";
   }
+
+  const post = res.data;
 
   return (
     <section className="py-6">
       <div className="container flex flex-col">
         <div className="flex-flex-col bg-card px-4 py-2">
-          <h3>{res.title}</h3>
+          <h3>{post.title}</h3>
           <div className="flex gap-3 text-foreground-lighter font-light text-sm items-center mt-2">
-            <Link href={`/forum/${res.forumId}`} className="underline">
-              f/{res.forum.name}
+            <Link href={`/forum/${post.forumId}`} className="underline">
+              f/{post.forum.name}
             </Link>
             &middot;
-            <Link href={`/profile/${res.posterId}`} className="underline">
+            <Link href={`/profile/${post.posterId}`} className="underline">
               {" "}
-              @{res.poster?.username ?? "deleted"}
+              @{post.poster?.username ?? "deleted"}
             </Link>
             <span className="text-xs">
-              {new Date(res.createdAt).toLocaleString()}
+              {new Date(post.createdAt).toLocaleString()}
             </span>
           </div>
         </div>
         <div className="py-2 flex flex-col gap-4">
-          <p className="text-md px-4 ">{res.body}</p>
-          <PostButtons post={res} />
+          <p className="text-md px-4 ">{post.body}</p>
+          <PostButtons post={post} />
         </div>
       </div>
     </section>
