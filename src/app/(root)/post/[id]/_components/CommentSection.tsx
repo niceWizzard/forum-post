54bf108dc78ee3b750b21cebcd3046e10c941de1
Richard <richard232manansala@gmail.com>
2024-07-10 12:35:08 +0800

    Added placeholder comment list

--- src/app/(root)/post/[id]/_components/CommentSection.tsx
+++ src/app/(root)/post/[id]/_components/CommentSection.tsx
@@ -2,6 +2,7 @@
 
 import { Post } from "@/server/db/schema/types";
 import { CommentForm } from "./CommentForm";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   post: Post;
@@ -15,6 +16,39 @@ export default function CommentSection({ post }: Props) {
         <button>Sort</button>
       </div>
       <CommentForm post={post} />
+      <div className="flex flex-col gap-3 divide-y divide-foreground-lighter">
+        <Comment post={post} />
+        <Comment post={post} />
+        <Comment post={post} />
+      </div>
     </section>
+  );
+}
+
+function Comment({ post }: { post: Post }) {
+  return (
+    <div className="flex flex-col gap-2 py-2">
+      <span className="text-sm">@username</span>
+      <span className="text-xs font-light text-foreground-lighter">
+        1 hour ago
+      </span>
+      <p className="text-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati,
+        perferendis quidem asperiores iusto dolorem excepturi, ea culpa nesciunt
+        quia voluptate aspernatur aliquid quibusdam magni molestias temporibus
+        modi, necessitatibus aut numquam quae magnam inventore deserunt et
+        doloremque? Cum magni libero ullam? Soluta esse amet porro quos itaque
+        blanditiis praesentium corrupti eveniet laudantium est corporis numquam
+        iste ipsa, natus nemo rerum, iusto, cum dolorum molestias. Quibusdam
+        praesentium vero possimus beatae totam. Quaerat soluta dignissimos quasi
+        autem accusantium, dolorum voluptates reiciendis! Nemo quo numquam
+        mollitia quam commodi debitis dolore, magnam totam asperiores sit ipsum
+        a ex fugiat similique consectetur velit ducimus beatae culpa.
+      </p>
+      <div className="flex gap-2">
+        <Button variant="ghost">Like</Button>
+        <Button variant="ghost">Reply</Button>
+      </div>
+    </div>
   );
 }
