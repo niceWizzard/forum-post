3050aebce3b01a306c1c7426dfe2d65e928003d0
Richard <richard232manansala@gmail.com>
2024-07-11 09:55:13 +0800

    Made a serverlessDb function creator

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,27 @@
 import { env } from "@/env/server.mjs";
 import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzleServerless } from "drizzle-orm/neon-serverless";
 import * as userSchemas from "@/server/db/schema";
 import * as commentSchemas from "@/server/db/schema/comment";
 import * as forumSchemas from "@/server/db/schema/forum";
 import * as postSchemas from "@/server/db/schema/post";
-import { neon } from "@neondatabase/serverless";
+import { neon, Pool } from "@neondatabase/serverless";
 
 const client = neon(env.NEON_DATABASE_URL);
 
+const schemas = {
+  ...userSchemas,
+  ...commentSchemas,
+  ...forumSchemas,
+  ...postSchemas,
+};
 export const db = drizzle(client, {
-  schema: {
-    ...userSchemas,
-    ...commentSchemas,
-    ...forumSchemas,
-    ...postSchemas,
-  },
+  schema: schemas,
 });
+
+export async function createCustomDb() {
+  const pool = new Pool({ connectionString: env.NEON_DATABASE_URL });
+  return drizzleServerless(await pool.connect(), {
+    schema: schemas,
+  });
+}
