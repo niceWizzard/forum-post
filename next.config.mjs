d60f5384771544fe06a4a731a3bec157e948bd56
Richard <richard232manansala@gmail.com>
2024-07-12 19:36:49 +0800

    Installed million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+import MillionLint from '@million/lint';
 await import("./src/env/client.mjs");
 await import("./src/env/server.mjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
