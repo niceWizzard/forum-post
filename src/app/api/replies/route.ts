a6e6beca2ce5fa2d9de365392a14f4fb766ab8a3
Richard <richard232manansala@gmail.com>
2024-07-13 17:26:11 +0800

    Created a route to get the replies for a comment

---
+++ src/app/api/replies/route.ts
@@ -0,0 +1,43 @@
+import { ApiError } from "@/server/apiErrors";
+import { NextApiRes, NextApiResponse } from "@/server/apiResponse";
+import { db } from "@/server/db";
+import { userTable } from "@/server/db/schema";
+import { commentTable } from "@/server/db/schema/comment";
+import { exposeUserType, ReplyComment } from "@/server/db/schema/types";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest
+): Promise<NextApiResponse<ReplyComment[]>> {
+  try {
+    const commentId = req.nextUrl.searchParams.get("commentId");
+    if (!commentId)
+      return NextApiRes.error({
+        message: "Missing commentId parameter",
+        code: ApiError.MissingParameter,
+      });
+    const res = await db
+      .select({
+        reply: { ...commentTable },
+        commenter: { ...userTable },
+      })
+      .from(commentTable)
+      .leftJoin(userTable, eq(userTable.id, commentTable.commenterId))
+      .where(eq(commentTable.replyToId, commentId));
+    return NextApiRes.success({
+      data: res.map((v) => ({
+        commenter: v.commenter ? exposeUserType(v.commenter) : null,
+        isLiked: null,
+        likeCount: 0,
+        ...v.reply,
+      })),
+    });
+  } catch (e) {
+    const err = e as Error;
+    return NextApiRes.error({
+      message: err.message,
+      code: ApiError.UnknownError,
+    });
+  }
+}
--- src/server/db/queries/comment.ts
+++ src/server/db/queries/comment.ts
@@ -1,10 +1,12 @@
 import { ApiRes, ApiResponse } from "@/server/apiResponse";
-import { RawComment } from "../schema/types";
+import { RawComment, ReplyComment } from "../schema/types";
 import { cache } from "react";
 import { ApiError } from "@/server/apiErrors";
 import { db } from "../index";
-import { eq } from "drizzle-orm";
+import { asc, desc, eq } from "drizzle-orm";
 import { commentTable } from "../schema/comment";
+import { getAuth } from "@/server/auth";
+import { userTable } from "../schema";
 
 export const getRawComment = cache(
   async (commentId: string): Promise<ApiResponse<RawComment>> => {
@@ -20,6 +22,43 @@ export const getRawComment = cache(
       }
       return ApiRes.success({
         data: comment,
+      });
+    } catch (e) {
+      const err = e as Error;
+      return ApiRes.error({
+        message: err.message,
+        code: ApiError.UnknownError,
+      });
+    }
+  }
+);
+
+export const getCommentReplies = cache(
+  async (commentId: string): Promise<ApiResponse<ReplyComment[]>> => {
+    try {
+      const { user } = await getAuth();
+      const res = await db
+        .select({
+          comment: { ...commentTable },
+          commenter: {
+            id: userTable.id,
+            username: userTable.username,
+            name: userTable.name,
+            email: userTable.email,
+          },
+        })
+        .from(commentTable)
+        .where(eq(commentTable.id, commentId))
+        .leftJoin(userTable, eq(userTable.id, commentTable.commenterId))
+        .orderBy(asc(commentTable.createdAt));
+
+      return ApiRes.success({
+        data: res.map((v) => ({
+          ...v.comment,
+          isLiked: null,
+          likeCount: 0,
+          commenter: v.commenter,
+        })),
       });
     } catch (e) {
       const err = e as Error;
