d1e164ad7045c419b4875188d7b04bf892cd94e8
Richard <richard232manansala@gmail.com>
2024-07-13 14:30:11 +0800

    Now uses the COUNT query to count the num of likes of the comment

--- src/server/db/queries/post.ts
+++ src/server/db/queries/post.ts
@@ -1,7 +1,7 @@
 import "server-only";
 import { db } from "../index";
 import { postLikeTable, postTable } from "../schema/post";
-import { and, asc, count, eq, desc, countDistinct } from "drizzle-orm";
+import { and, asc, count, eq, desc, countDistinct, sql } from "drizzle-orm";
 import { userTable } from "../schema";
 import {
   exposeUserType,
@@ -50,13 +50,21 @@ export const getPostById = cache(
           .leftJoin(commentTable, eq(commentTable.postId, postTable.id))
           .groupBy(userTable.id, forumTable.id, postTable.id),
         db
-          .select()
+          .select({
+            user: { ...userTable },
+            comment: { ...commentTable },
+            likeCount: sql<number>`COUNT(DISTINCT ${commentLikeTable.userId}) AS like_count`,
+            isLiked: sql<boolean>`SUM( CASE
+            WHEN ${commentLikeTable.userId} = ${
+              user?.id ?? "11111111-1111-1111-1111-1ce992f5e2db"
+            } THEN 1 ELSE 0 END) > 0`,
+          })
           .from(commentTable)
           .where(eq(commentTable.postId, id))
           .orderBy(
             sort == "newest"
               ? orderFunc(commentTable.createdAt)
-              : orderFunc(commentTable.likeCount),
+              : orderFunc(sql`like_count`),
             asc(commentTable.createdAt)
           )
           .limit(10)
@@ -64,14 +72,9 @@ export const getPostById = cache(
           .leftJoin(userTable, eq(userTable.id, commentTable.commenterId))
           .leftJoin(
             commentLikeTable,
-            and(
-              eq(commentLikeTable.commentId, commentTable.id),
-              eq(
-                commentLikeTable.userId,
-                user?.id ?? "11111111-1111-1111-1111-1ce992f5e2db" // Some nonsense uuid just to not have an error.
-              )
-            )
-          ),
+            eq(commentLikeTable.commentId, commentTable.id)
+          )
+          .groupBy(commentTable.commenterId, userTable.id, commentTable.id),
       ]);
 
       if (res.length == 0)
@@ -102,13 +105,11 @@ export const getPostById = cache(
           forum,
           isLiked,
           initialComments: rawComments.map(
-            ({ user, comment, comment_like }) => {
-              let isLiked = null;
-              if (user) {
-                isLiked = !!comment_like;
-              }
+            ({ user, comment, likeCount, ...v }) => {
+              let isLiked = user ? v.isLiked : null;
               return {
                 commenter: user,
+                likeCount,
                 isLiked,
                 ...comment,
               };
