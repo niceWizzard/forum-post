2e580aac9b2e05fc46a71e89e630163352bec674
Richard <richard232manansala@gmail.com>
2024-07-15 18:26:10 +0800

    removed millionlint

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-import MillionLint from '@million/lint';
 await import("./src/env/client.mjs");
 await import("./src/env/server.mjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-export default MillionLint.next({
-  rsc: true
-})(nextConfig);
+export default nextConfig;
