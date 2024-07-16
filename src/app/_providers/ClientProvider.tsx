fcb8e6e7ed311485d98ad4f359a81f5088f8731b
Richard <richard232manansala@gmail.com>
2024-07-16 13:15:21 +0800

    Added the client providers for trpc

--- src/app/_providers/ClientProvider.tsx
+++ src/app/_providers/ClientProvider.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { trpc } from "../_trpc/client";
+import { httpBatchLink } from "@trpc/client";
+import { env } from "@/env/client.mjs";
+
 export default function ClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  const { queryClient, trpcClient } = useMemo(
+    () => ({
+      queryClient: new QueryClient(),
+      trpcClient: trpc.createClient({
+        links: [
+          httpBatchLink({
+            url: `${env.PUBLIC_BASE_URL}api/trpc`,
+          }),
+        ],
+      }),
+    }),
+    []
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 }
