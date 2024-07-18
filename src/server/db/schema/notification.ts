395e864a43e370c33d055e510929aaf0dfeb55bd
Richard <richard232manansala@gmail.com>
2024-07-18 09:51:43 +0800

    Removed the type enum for notification

--- src/server/db/schema/notification.ts
+++ src/server/db/schema/notification.ts
@@ -1,11 +1,6 @@
 import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { userTable } from ".";
 
-export const notificationType = pgEnum("notification_type", [
-  "forumAssignAdmin",
-  "forumAssignOwner",
-]);
-
 export const notificationTable = pgTable("notification", {
   id: uuid("id").defaultRandom().primaryKey(),
   userId: uuid("user_id")
@@ -14,7 +9,6 @@ export const notificationTable = pgTable("notification", {
       onDelete: "cascade",
     }),
   message: varchar("link_to", { length: 5024 }).notNull(),
-  type: notificationType("type").notNull(),
   linkTo: varchar("link_to", { length: 5024 }).notNull(),
   readAt: timestamp("read_at"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
